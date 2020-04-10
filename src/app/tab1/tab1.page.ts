64337e69c30afb9e9f839c9436f81418a16541a9
Zach <bridges.zach@gmail.com>
2020-04-09 23:48:09 -0400

    class 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { DataService } from "../services/data.service";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"],
 })
 export class Tab1Page {
+  constructor(private data: DataService) {}
 
-  constructor() {}
-
+  getMessages() {
+    console.log(this.data.getAllMessages());
+    return this.data.getAllMessages();
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Message } from "../models/message";
+import { SharedService } from "../services/shared.service";
+import { DataService } from "../services/data.service";
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"],
 })
 export class Tab2Page {
+  message: Message = new Message();
 
-  constructor() {}
+  constructor(private shared: SharedService, private data: DataService) {}
 
+  onPost() {
+    this.message.from = this.shared.userName;
+    this.message.createdOn = new Date();
+    this.data.saveMessage(this.message);
+    // clear form
+    this.message = new Message();
+  }
 }
